d820ac7396af43f3714ecf81e5fb2d724165c6c0
Frank Young <yangjunalns@qq.com>
2015-06-02 15:52:55 +0800

    change
    修改

---
+++ dome/web1/js/login.js
@@ -0,0 +1,236 @@
+
+
+/*  	表单验证
+*=================================================================================
+*/
+
+	var ipt = document.getElementsByName("in");
+		 var name= ipt[0];
+		 var email = ipt[2];
+ 		 
+		 var wraing = document.getElementsByName('wraing');//提示标签
+		 var name_msg = wraing[0];
+		 var pass_msg = wraing[1];
+		 var pass2_msg = wraing[2];
+		 var email_msg = wraing[3];
+		 nosub = [];//用于判断表单是否提交
+
+//用户名 交互
+		 ipt[0].onfocus = function(){ 
+
+		 	name_msg.style.visibility="visible";
+		 	name_msg.innerHTML="请输入6-20个字符,字母或,数字,汉字或下划线";
+			
+		 }//得到焦点
+		
+		 ipt[0].onblur = function(){ 
+
+		 	var re = /[^\w\u4e00-\u9fa5]/g;
+		 	if(re.test(this.value)){ 
+		 		name_msg.innerHTML="含有不合法字符！";
+		 	}else if(this.value ==""){ 
+		 		name_msg.innerHTML="不能为空！";
+		 	}
+		 	else if(this.value.length>20){ 
+		 		name_msg.innerHTML="长度超过20！";
+		 	}else if(this.value.length<6){ 
+		 		name_msg.innerHTML="长度小于6！";
+		 	}else{ 
+		 		name_msg.innerHTML="OK! ";
+		 		nosub[0] = true;
+		 	}
+
+
+		 }//失去焦点
+
+//密码 交互
+		 ipt[1].onfocus = function(){ 
+
+		 	pass_msg.style.visibility="visible";
+		 	pass_msg.innerHTML="请输入6-20个字符,字母或,数字";
+			
+		 }//得到焦点
+		
+		 ipt[1].onblur = function(){ 
+
+		 	var word = /[^a-zA-Z\d]/g;
+		 	if(word.test(this.value)){ 
+		 		pass_msg.innerHTML="含有不合法字符！";
+		 	}else if(this.value ==""){ 
+		 		pass_msg.innerHTML="不能为空！";
+		 	}
+		 	else if(this.value.length>20){ 
+		 		pass_msg.innerHTML="长度超过20！";
+		 	}else if(this.value.length<6){ 
+		 		pass_msg.innerHTML="长度小于6！";
+		 	}else{ 
+		 		pass_msg.innerHTML="OK! ";
+		 		ipt[2].removeAttribute("disabled");
+		 		nosub[1]  = true;
+		 	}
+
+
+		 }//失去焦点
+
+//再次密码 交互
+		 ipt[2].onfocus = function(){ 
+
+		 	pass2_msg.style.visibility="visible";
+		 	pass2_msg.innerHTML="请再次输入!";
+			
+		 }//得到焦点
+		
+		 ipt[2].onblur = function(){ 
+
+		 	if(this.value != ipt[1].value){ 
+		 		pass2_msg.innerHTML="两次密码不一样,请重新输入!";
+		 	}else{ 
+		 		pass2_msg.innerHTML="OK!";
+		 		nosub[2] = true;
+		 	}
+
+
+		 }
+
+//邮箱验证
+		  ipt[3].onfocus = function(){ 
+		  	email_msg.style.visibility="visible";
+		 	
+		 	email_msg.innerHTML="请输入你的邮箱";
+			
+		 }//得到焦点
+		 
+		 ipt[3].onblur = function(){ 
+		 	var eml = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+		 	
+		 	if(eml.test(this.value)){ 
+		 		email_msg.innerHTML="OK!";
+		 		nosub[3] = true;
+		 		
+		 	}else if(this.value ==""){ 
+		 		email_msg.innerHTML="不能为空！";
+		 	}else{ 
+		 		email_msg.innerHTML="邮箱格式不对！!";
+		 	}
+
+
+		 }
+/*
+*=================================================================================
+*/
+
+
+
+
+
+
+
+/*	弹出框
+*=================================================================================
+*/
+	function sub(){ 
+		 var nameVal = ipt[0].value;
+		 var emailVal = ipt[3].value;
+		 var sex = document.getElementsByName("sexs");
+		
+		 if (sex[0].checked== true) { 
+		 	alert('姓名：'+nameVal+'email:'+emailVal+' 性别：男'); 
+		 	nosub[4] = true;
+		 }else if (sex[1].checked== true){ 
+		 	alert('姓名：'+nameVal+'email:'+emailVal+' 性别：女'); 
+		 	nosub[5] = true;
+		 }else{ 
+		 	
+		 	alert('填写不正确');
+		 	
+		 }
+		 
+	}
+/*
+*=================================================================================
+*/
+
+
+
+
+
+/*	提交表单判断函数
+*=================================================================================
+*/
+
+
+	function subFun(){ 
+		var num=0;
+		for(var i=0; i<=nosub.length;i++){ 
+			if (nosub[i] == true){ 
+				num++;
+			}
+		}
+		if(num == 5){ 
+			return true;
+		}
+		else { 
+			return false;
+		}
+
+	}
+
+
+
+/*	
+*=================================================================================
+*/
+
+
+
+
+
+
+/*	弹出层
+*=================================================================================
+*/
+	var aph = document.getElementById('aph');
+	var showme = document.getElementById('showMe');
+	var close = document.getElementById('close');
+	var aphHeight =document.body.scrollHeight;
+
+	function show() {
+		
+		showme.style.visibility="visible";
+		aph.style.height=aphHeight+'px';
+		aph.style.visibility="visible";
+
+	}
+	aph.onclick =function(){ 
+		var quite = confirm('你是否不继续填写了？');
+		if(quite == true){
+			showme.style.visibility="hidden";
+			aph.style.visibility="hidden";
+			name_msg.style.visibility="hidden";
+			pass_msg.style.visibility="hidden";
+			pass2_msg.style.visibility="hidden";
+			email_msg.style.visibility="hidden";
+		}
+	}
+	close.onclick =function(){ 
+		var quite = confirm('你是否不继续填写了？');
+		if(quite == true){
+			showme.style.visibility="hidden";
+			aph.style.visibility="hidden";
+			name_msg.style.visibility="hidden";
+			pass_msg.style.visibility="hidden";
+			pass2_msg.style.visibility="hidden";
+			email_msg.style.visibility="hidden";
+		}
+	}
+/*
+*=================================================================================
+*/
+
+
+
+
+
+
+
+
---
+++ dome/web1/js/user.js
@@ -0,0 +1,170 @@
+window.onload =func1;
+//window.onload =remove1;
+
+/*
+*==================================================
+*	弹出窗口函数
+*
+*==================================================
+*/
+function func1(){
+		var aph = document.getElementById('aph');
+		var close = document.getElementById('close');
+		var aphHeight =document.body.scrollHeight;
+		var showme = document.getElementById('showMe');
+		var tan = document.getElementById("tan");
+	tan.onclick = function(){ 
+		addshow();
+		return false;
+	}
+	aph.onclick =function(){ 
+		var quite = confirm('你是否不继续填写了？');
+		if(quite == true){
+			addClose();//关闭函数
+		}
+	}
+	close.onclick =function(){ 
+		var quite = confirm('你是否不继续填写了？');
+		if(quite == true){
+			addClose();	
+		}
+	}
+
+	//显示弹窗的函数
+	this.addshow =function  show() {	
+		showme.style.visibility="visible";//显示内容窗口
+		aph.style.height=aphHeight+'px';//设置阴影高度和文本高度一样
+		aph.style.visibility="visible";//显示阴影
+	}//关闭弹窗的函数
+	this.addClose = function clo(){ 
+		showme.style.visibility="hidden";
+		aph.style.visibility="hidden";
+	}
+
+	return false;
+	
+}
+
+
+/*
+*==================================================
+*	添加文章函数
+*
+*==================================================
+*/
+
+
+	var addArtic = document.getElementById("addArtic");
+	var input = addArtic.getElementsByTagName("input");
+	var textarea = addArtic.getElementsByTagName("textarea");
+	var title = input[0].value;
+	var author = input[1].value;
+	var description = textarea[0].value;
+	var content = textarea[1].value;
+	var addall = document.getElementById("addAll");
+
+	var success = document.getElementById("success");
+	success.onclick = function(){ 
+
+		add(input[0].value,textarea[0].value);
+		
+	}
+
+	function add(title,description){ 
+	
+		if(title != ""&&description !=""){
+			
+			var h2node = document.createElement("h2");
+			var h2textnode = document.createTextNode(title);
+			h2node.appendChild(h2textnode);
+			addall.appendChild(h2node);
+	
+		var pnode = document.createElement("p");
+		var ptextnode = document.createTextNode(description);
+		pnode.appendChild(ptextnode);
+		addall.appendChild(pnode);
+		addbt();//增加删除，修改按钮
+		alert('发布文章成功！');
+		var addclo = new func1();
+		addclo.addClose();	
+		cls();
+		
+		}else{
+			alert("标题或描述不能为空！")
+		}
+	}
+//删除文章
+function remove1(a){ 
+	//element.nextSibling
+	//element.previousSibling
+	//element.removeChild()
+	//var a = addall.getElementsByTagName("a");
+	//a[1].onclick = function(){ 
+		//var h2 = document.getElementById("h2");
+		var p = a.previousSibling.previousSibling;
+		var h2=p.previousSibling.previousSibling;
+		var a2 =a.nextSibling.nextSibling;
+		h2.remove();
+		p.remove();
+		a2.remove();
+		a.remove();
+		//h2.firstChild.firstChild.nodeValue="";
+		//p.firstChild.nodeValue="";
+		//a2.firstChild.nodeValue="";
+		//a.firstChild.nodeValue="";
+	//}
+	return false;
+}
+//修改文章
+function re(b){ 
+
+	//var a = addall.getElementsByTagName("a");
+	//a[1].onclick = function(){ 
+		//var h2 = document.getElementById("p");
+		var a1 = b.previousSibling.previousSibling;
+		var p1=a1.previousSibling.previousSibling;
+		var h21=p1.previousSibling.previousSibling;
+		var tit = p1.firstChild.nodeValue;
+		var des = h21.firstChild.firstChild.nodeValue;
+	
+		//创建对象
+		var addclo = new func1();
+		var addsho =  addshow();
+
+		
+
+	
+	//}
+
+}
+
+
+//添加修改删除按钮函数
+function addbt(){ 
+	var remonode = document.createElement("a");
+	var remotextnode = document.createTextNode("删除");
+	remonode.setAttribute("onclick","remove1(this)");
+	remonode.setAttribute("href","#");
+	remonode.appendChild(remotextnode);
+	addall.appendChild(remonode);
+
+	var renode = document.createElement("a");
+	var retextnode = document.createTextNode("修改");
+	renode.setAttribute("onclick","re(this)");
+	renode.setAttribute("href","#");
+	renode.appendChild(retextnode);
+	addall.appendChild(renode);
+
+}
+
+//清空表单数据
+	function cls(){ 
+		input[0].value="";
+		input[1].value="";
+		textarea[0].value="";
+		textarea[1].value="";
+	}
+
+
+ 
+
